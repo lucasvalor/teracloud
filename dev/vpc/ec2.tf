6b2c5b8b7c218c523c075a3279c053c0b042be3d
Lucas Valor <lucasvalor@gmail.com>
2019-04-25 20:30:51 -0300

    Add security group

--- dev/vpc/ec2.tf
+++ dev/vpc/ec2.tf
@@ -1,4 +1,29 @@
 resource "aws_instance" "MyEC2Instance-1" {
-  ami           = "ami-0de53d8956e8dcf80"
-  instance_type = "t2.micro"
+  ami             = "${var.ami}"
+  instance_type   = "${var.ec2_type}"
+  key_name        = "EC2-us-east-1"
+  security_groups = ["${aws_security_group.allow_ssh.name}"]
+
+  tags {
+    Name = "MyEC2"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "This allows SSH connections"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
--- dev/vpc/variables.tf
+++ dev/vpc/variables.tf
@@ -2,3 +2,11 @@ variable "region" {
   description = "The AWS region we are going to use"
   default     = "us-east-1"
 }
+
+variable "ec2_type" {
+  default = "t2.micro"
+}
+
+variable "ami" {
+  default = "ami-0de53d8956e8dcf80"
+}
