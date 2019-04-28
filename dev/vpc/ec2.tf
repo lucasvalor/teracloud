213a43c361ee28618a919c68a4f3de23b3cbbcb8
Lucas Valor <lucasvalor@gmail.com>
2019-04-28 18:44:06 -0300

    Use aws ami data to get the latest ubuntu image

--- dev/vpc/ec2.tf
+++ dev/vpc/ec2.tf
@@ -1,5 +1,20 @@
+data "aws_ami" "latest_ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-disco-19.04-amd64-server-*"]
+  }
+
+  filter {
+      name   = "virtualization-type"
+      values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "MyEC2Instance-1" {
-  ami             = "${var.ami}"
+  ami             = "${data.aws_ami.latest_ubuntu.id}"
   instance_type   = "${var.ec2_type}"
   key_name        = "${aws_key_pair.EC2_key_pair.key_name}"
   security_groups = ["${aws_security_group.allow_ssh.name}"]
@@ -25,6 +40,10 @@ resource "aws_security_group" "allow_ssh" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "MyEC2SG"
   }
 }
 
--- dev/vpc/variables.tf
+++ dev/vpc/variables.tf
@@ -8,11 +8,6 @@ variable "ec2_type" {
   default     = "t2.micro"
 }
 
-variable "ami" {
-  description = "Specify the image to use"
-  default     = "ami-0de53d8956e8dcf80"
-}
-
 variable "public_key" {
   description = "The public key to SSH EC2 instance"
 }
