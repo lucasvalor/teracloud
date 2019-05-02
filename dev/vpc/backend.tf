7094371db8077413b751167de2b2082f0e600fda
Lucas Valor <lucasvalor@gmail.com>
2019-05-01 22:35:44 -0300

    Add Backend directory

--- dev/vpc/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "lucasvalor"
-    encrypt = "true"
-    key     = "teracloud/dev/terraform_dev.tfstate"
-    region  = "us-east-1"
-  }
-}
--- dev/vpc/output.tf
+++
@@ -1,4 +0,0 @@
-output "ec2-public-ip" {
-  description = "This is the public IP address of the EC2 instance"
-  value       = "${aws_instance.MyEC2Instance-1.public_ip}"
-}
